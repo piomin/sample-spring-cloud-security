d575870d586d399a58364e0610350ec218f99533
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-02-05 23:00:34 +0100

    adding auth service

---
+++ auth-service/src/main/java/pl/piomin/services/auth/AuthApplication.java
@@ -0,0 +1,15 @@
+package pl.piomin.services.auth;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(AuthApplication.class).web(true).run(args);
+	}
+	
+}
