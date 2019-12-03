aee692a0f9da935e88c5d55bc4a1ddc3d793b4fb
minkowp <piotr.minkowski@play.pl>
2019-12-03 16:48:19 +0100

    fixing

--- account-service/src/main/java/pl/piomin/services/account/SSLConfigServiceBootstrapConfiguration.java
+++ account-service/src/main/java/pl/piomin/services/account/SSLConfigServiceBootstrapConfiguration.java
@@ -1,18 +1,6 @@
 package pl.piomin.services.account;
 
-import java.io.File;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
@@ -24,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
 
 @Configuration
 public class SSLConfigServiceBootstrapConfiguration {
