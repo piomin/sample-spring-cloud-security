85072e8fa5b33f3d10a7fd6ca7eb636a2406b012
piomin <piotr.minkowski@gmail.com>
2021-05-24 08:26:47 +0200

    adding gateway security #2

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package pl.piomin.services.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -10,12 +11,15 @@ import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.VaultTemplate;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class } )
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+    @Value("vault.token")
+    private String vaultToken;
 
     @Bean
     KeyResolver authUserKeyResolver() {
@@ -27,7 +31,7 @@ public class GatewayApplication {
     VaultTemplate vaultTemplate() {
         VaultEndpoint e =  new VaultEndpoint();
         e.setScheme("http");
-        VaultTemplate template = new VaultTemplate(e, new TokenAuthentication("s.FpDxOxie9aEkvD4qPg5v2VkW"));
+        VaultTemplate template = new VaultTemplate(e, new TokenAuthentication(vaultToken));
         return template;
     }
 
--- gateway-service/src/main/java/pl/piomin/services/gateway/config/SecurityConfig.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package pl.piomin.services.gateway.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -11,20 +9,20 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
-@Slf4j
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         SubjectDnX509PrincipalExtractor principalExtractor =
                 new SubjectDnX509PrincipalExtractor();
         principalExtractor.setSubjectDnRegex("CN=(.*?)(?:,|$)");
 
-        return http.httpBasic().disable().authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+        return http.csrf().disable()
+                .authorizeExchange(exchanges ->
+                        exchanges.anyExchange().authenticated())
                 .x509()
                     .principalExtractor(principalExtractor)
                 .and()
